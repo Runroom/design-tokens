fa326148ec79ed8fc116d8321d43d91cf44fcafd
Jose <jose@runroom.com>
2020-05-15 11:28:02 +0200

    Changes Types export

---
+++ src/types/index.js
@@ -0,0 +1,11 @@
+import getBreakpoints from './getBreakpoints';
+import getColors from './getColors';
+import getSpacing from './getSpacing';
+import getTypography from './getTypography';
+
+export {
+    getBreakpoints,
+    getColors,
+    getSpacing,
+    getTypography
+};
