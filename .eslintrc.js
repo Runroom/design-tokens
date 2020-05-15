268ae4c1ad15dd11aef6a51d95f37d397515e852
Jose <jose@runroom.com>
2020-05-15 14:10:43 +0200

    Adds eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['import', 'prettier'],
+  env: {
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+      globalReturn: true
+    }
+  },
+  overrides: [
+    {
+      files: '*.spec.js',
+      rules: {
+        'no-unused-expressions': 'off',
+        'no-undef': 'off'
+      }
+    }
+  ],
+  parser: 'babel-eslint',
+  globals: {
+    document: false,
+    escape: false,
+    navigator: false,
+    unescape: false,
+    window: false,
+    location: false,
+    describe: true,
+    before: true,
+    after: true,
+    it: true,
+    expect: true,
+    sinon: true,
+    purejs: true
+  }
+};
