0a5f4bda01b34f890af5626c0d981ef8cf940cff
Jose <jose@runroom.com>
2020-05-25 18:26:02 +0200

    Figma sync works from index file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// const figmaCli = require('src/figma-cli.js');
-// const styleDictionary = require('src/style-dictionary.js');
+require = require("esm")(module /*, options */);
+const figmaCli = require('./src/figma-cli.js');
+const styleDictionary = require('./src/style-dictionary.js');
 const CONFIG_FILE_DEFAULT = 'designtokens.config.json';
 
 function designTokens(argv) {
   const command = argv._[0];
-  const configFile = argv['config-file'] || CONFIG_FILE_DEFAULT_PATH;
-
-  console.log(configFile);
+  const configFile = `${__dirname}/${argv['config-file'] || CONFIG_FILE_DEFAULT}`;
 
   switch (command) {
     case 'figma':
-      console.log('Sync');
+      figmaCli(configFile);
       break;
     case 'build':
-
+      styleDictionary(configFile)
       break;
     default:
       console.log('DEFAULT');
