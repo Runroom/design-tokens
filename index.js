ad800787352fe75b43ee4ed65bf3685b91e12eb1
Jose <jose@runroom.com>
2020-05-29 18:16:45 +0200

    gets root app project directory for config file loading

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require = require("esm")(module /*, options */);
+const path = require('path');
+
 const figmaCli = require('./src/figma-cli.js');
 const styleDictionary = require('./src/style-dictionary.js');
 const CONFIG_FILE_DEFAULT = 'designtokens.config.json';
 
+const appDir = path.resolve(__dirname).split('/node_modules')[0];
+
 function designTokens(argv) {
   const command = argv._[0];
-  const configFile = `${__dirname}/${argv['config-file'] || CONFIG_FILE_DEFAULT}`;
+  const configFile = `${appDir}/${argv['config-file'] || CONFIG_FILE_DEFAULT}`;
 
   switch (command) {
     case 'figma':
