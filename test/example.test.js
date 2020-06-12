ad6090b70afdbf423fa32332a8709612082d42c9
Jose <jose@runroom.com>
2020-06-12 10:01:44 +0200

    Adds mocha

---
+++ test/example.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Simple Math Test', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
