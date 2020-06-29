fa5197f09173b606c70aa64fa806b8a1694bece5
Jose <jose@runroom.com>
2020-06-29 17:38:44 +0200

    Iterates async order

--- test/api.test.js
+++ test/api.test.js
@@ -19,29 +19,9 @@ describe('Figma connection', () => {
       fetch(FETCH_URL, FETCH_DATA)
         .then(response => response.json())
         .then(styles => {
-          console.log('Hola');
-          // done();
           expect(styles.status).to.not.equal(403);
           expect(styles.status).to.not.equal(404);
           done();
-          // if (styles.status !== 403 && styles.status !== 404) {
-          //   const figmaTree = styles.document.children.filter(page => page.name === pageName);
-
-          //   if (figmaTree.length === 0) throw new Error(`There is no page called '${pageName}'`);
-          //   console.log(` Parsing Figma tokens...`);
-
-          //   Promise.all([
-          //     genFile('color', generateTokens('Colors', figmaTree[0].children, getColors), outDir),
-          //     genFile('spacings', generateTokens('Spacings', figmaTree[0].children, getSpacings), outDir),
-          //     genFile('typography', generateTokens('Typography', figmaTree[0].children, getTypography), outDir),
-          //     // genFile('breakpoint', generateTokens('Breakpoints', figmaTree[0].children, getBreakpoints), outDir)
-          //   ]).then(() => {
-          //     resolve();
-          //   }).catch(err => {
-          //     reject();
-          //     throw new Error(`\x1b[31m\n\n${emojis.error} ${err}\n`);
-          // });
-          // }
         })
         .catch(err => {
           done(err);
