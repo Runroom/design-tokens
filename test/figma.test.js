65ca8ceceb9533dfafa3670ec8f795e8f3f02327
Jose <jose@runroom.com>
2020-06-30 11:39:48 +0200

    Moves json parsing tests to api connection file

--- test/figma.test.js
+++ test/figma.test.js
@@ -3,7 +3,10 @@
 require = require("esm")(module /*, options */);
 const fetch = require('node-fetch');
 const expect = require('chai').expect;
-const assert = require('chai').assert;
+const parser = require('../src/figma-parser');
+const decorators = require('../src/decorators');
+const mockJson = require('./data')[0].children;
+// const assert = require('chai').assert;
 
 const FETCH_URL = `https://api.figma.com/v1/files/laOdxGSyWrN0Of2HpeOX7L`;
 const FETCH_DATA = {
@@ -13,24 +16,99 @@ const FETCH_DATA = {
   }
 };
 const PAGE_NAME = '🔄 Design Tokens v2';
+let figmaJson;
+let figmaTree;
 
-describe('Figma project fetching', () => {
-  let figmaJson;
+before(async () => {
+  await fetch(FETCH_URL, FETCH_DATA)
+    .then(response => response.json())
+    .then(response => {
+      figmaJson = response;
+    });
+});
 
-  before(async () => {
-    await fetch(FETCH_URL, FETCH_DATA)
-      .then(response => response.json())
-      .then(response => {
-        figmaJson = response;
+describe('Figma connection', () => {
+  describe('Json fetching', () => {
+    it('Project exists', async () => {
+      expect(figmaJson.status).to.not.equal(403);
+      expect(figmaJson.status).to.not.equal(404);
+    });
+    it('Page exists', () => {
+      expect(figmaJson.document).to.exist;
+      figmaTree = figmaJson.document.children.filter(page => page.name === PAGE_NAME);
+      expect(figmaTree.length).to.be.greaterThan(0);
+    });
+  });
+
+  describe('Json parsing', () => {
+    // const mockJson = figmaTree[0].children;
+
+    describe('Color parser', () => {
+      const colors = parser.filterArtboardElements('Colors', mockJson);
+      const tokens = parser.generateTokens('Colors', mockJson, decorators.getColors);
+      const color = tokens['colors'][Object.keys(tokens['colors'])[0]].value;
+
+      it('filtered artboard is array', () => {
+        expect(colors).to.be.a('array');
       });
-  });
-  it('Project exists', async () => {
-    expect(figmaJson.status).to.not.equal(403);
-    expect(figmaJson.status).to.not.equal(404);
-  });
-  it('Page exists', () => {
-    expect(figmaJson.document).to.exist;
-    const figmaTree = figmaJson.document.children.filter(page => page.name === PAGE_NAME);
-    expect(figmaTree.length).to.be.greaterThan(0);
+      it('tokens is object', () => {
+        expect(tokens).to.be.a('object');
+        expect(tokens['colors']).to.be.a('object');
+      });
+      it('value is string', () => {
+        expect(color).to.be.a('string');
+      });
+      it('value is hexadecimal', () => {
+        expect(/#([0-9A-F]{3}|[0-9A-F]{6})/i.test(color)).to.be.true;
+      });
+    });
+
+    describe('Spacings parser', () => {
+      const spacings = parser.filterArtboardElements('Spacings', mockJson);
+      const tokens = parser.generateTokens('Spacings', mockJson, decorators.getSpacings);
+      const spacing = tokens['spacings'][Object.keys(tokens['spacings'])[0]].value;
+
+      it('filtered artboard is array', () => {
+        expect(spacings).to.be.a('array');
+      });
+      it('tokens is object', () => {
+        expect(tokens).to.be.a('object');
+        expect(tokens['spacings']).to.be.a('object');
+      });
+      it('value is string', () => {
+        expect(spacing).to.be.a('string');
+      });
+      it('value is pixelated', () => {
+        expect(/([0-9]+)px/.test(spacing)).to.be.true;
+      });
+    });
+
+    describe('Typography parser', () => {
+      const typography = parser.filterArtboardElements('Typography', mockJson);
+      const tokens = parser.generateTokens('Typography', mockJson, decorators.getTypography);
+      const text = tokens['typography'][Object.keys(tokens['typography'])[0]];
+
+      it('filtered artboard is array', () => {
+        expect(typography).to.be.a('array');
+      });
+      it('tokens is object', () => {
+        expect(tokens).to.be.a('object');
+        expect(tokens['typography']).to.be.a('object');
+      });
+      it('has correct props', () => {
+        expect(text.fontFamily).to.not.be.undefined;
+        expect(text.fontSize).to.not.be.undefined;
+        expect(text.lineHeight).to.not.be.undefined;
+        expect(text.lineHeightRelative).to.not.be.undefined;
+        expect(text.fontWeight).to.not.be.undefined;
+      });
+      it('props have valid types', () => {
+        expect(/([0-9]+)px/.test(text.fontSize.value)).to.be.true;
+        expect(/([0-9]+)px/.test(text.lineHeight.value)).to.be.true;
+        expect(/[1-9]{1}00/.test(text.fontWeight.value)).to.be.true;
+        expect(text.fontFamily.value).to.be.a('string');
+        expect(text.lineHeightRelative.value).to.be.a('number');
+      });
+    });
   });
 });
