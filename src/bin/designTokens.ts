60f288a2bd8aee52f465d299c411e4dff1052fc4
Imar Abreu <imar@runroom.com>
2023-12-15 17:46:50 +0000

    Refactor

--- src/bin/designTokens.ts
+++ src/bin/designTokens.ts
@@ -2,11 +2,11 @@
 
 import parserRuntime from 'yargs-parser';
 import designTokens from '@/index.ts';
-import { logError, configFileParser } from '@/functions';
+import { configFileParser, logError } from '@/functions';
 import fs from 'fs';
 
 const args = parserRuntime(process.argv.slice(2));
 
 configFileParser(args, fs)
-  .then(config => designTokens(args, config))
+  .then(config => designTokens(config))
   .catch(err => logError(err));
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { buildStyleDictionary } from '@/styleDictionary/StyleDictionary.ts';
-import { Arguments } from 'yargs-parser';
+import { buildStyleDictionary } from '@/styleDictionary/styleDictionary.ts';
 import { ParseConfig } from '@/types/designTokens';
 import { figmaApiConnection } from '@/api';
 import { createJsonTokenFiles, EMOJIS, log } from '@/functions';
 
-const designTokens = (args: Arguments, config: ParseConfig) => {
+const designTokens = (config: ParseConfig) => {
   const { settings } = config;
 
   figmaApiConnection(settings).then(async generatedTokens => {
